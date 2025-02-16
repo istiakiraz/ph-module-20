2f8d0256caa9fb2521f81150b55003449d064d4d
istiakiraz <istiakiraz@gmail.com>
2025-02-17 00:26:51 +0600

    first commit

---
+++ case.js
@@ -0,0 +1,15 @@
+// module 20.2
+
+const school = "Feni govt. pilot high school"
+
+console.log(school.toUpperCase()); // toLowerCase then - '()' must
+
+const sub = "bangla"
+const book = "Bangla"
+
+if(sub.toLowerCase() === book.toLowerCase()){
+    console.log("i love this");
+}
+else{
+    console.log("i hate this");
+}
---
+++ string.js
@@ -0,0 +1,9 @@
+// module 20.1
+
+const country = "bangladesh";
+const division = "Newkhale";
+const district = `B-Baria`
+
+const capital = "Dhaka"
+console.log(capital.length);
+console.log(capital[2]); // string index number find
