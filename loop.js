b8f03d60010a102b9c6830c80b119b33a048a2c9
istiakiraz <istiakiraz@gmail.com>
2025-02-17 04:40:47 +0600

    added a file

---
+++ loop.js
@@ -0,0 +1,35 @@
+// module 20.9 
+
+const mobile = {
+    brand: "iphone",
+    price: 25000,
+    color: "black",
+    camera : "36px",
+    isnew : true
+}
+
+// for of : array
+// for in : object
+
+for(const prop in mobile){
+    console.log(prop);
+    console.log(mobile[prop]);
+}
+
+// different looping use for of
+
+const key =Object.keys(mobile)
+console.log(key);
+
+for(const ky of key){
+    console.log(ky , ':' ,(mobile[ky]));
+    
+}
+
+// extra ----------
+
+const pen = new Object()
+console.log(pen);
+
+const light = Object.create({});
+console.log(light);
